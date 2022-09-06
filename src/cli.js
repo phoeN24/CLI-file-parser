a3183b356827a7a853a7309f6e6be773ad6d8933
phoeN24 <phoenix@kilodelta.com>
2022-09-05 17:33:54 -0700

    calls formatPipeComma with correct input

--- src/cli.js
+++ src/cli.js
@@ -7,35 +7,50 @@ const cache = {};
 
 //parse each input file, read data and pass data to be formated
 function fileParser (){
-  fs.readdir(folder, {encoding: 'utf8'}, function (err, files) { 
+  fs.readdir(folder, {encoding: 'utf8'}, (err, files)  => { 
   if (err) { 
     console.log("ERROR", err); throw err; 
   } else {
     //reads file and returns file's data as a string
     files.forEach(file => {
       fs.readFile(path.resolve('./input-files' , file),{encoding:'utf8', flag:'r'}, 
-        function(err, data) {
+        function (err, data) {
         if(err) 
           console.log(err);
         else
           //call formatData function passing in the string data
           return formatData(data);
         });      
-    })
-  }
-})
+      });
+    };
+  });
 };
 
 function formatData(data){
 //creates an array of 3 strings and stores them under arrOfStrings label˜
   let arrOfStrings =  data.split(/\r?\n/)
   console.log('test',arrOfStrings)
+
+  for(let str = 0; str < arrOfStrings.length; str++){
+    for(let i = 0; i < arrOfStrings[str].length; i++){
+      if (arrOfStrings[str][i]  === ',' || arrOfStrings[str][i] === '|'){
+        formatPipeComma(arrOfStrings[str])
+        break;
+      }
+    }
+  }
 //remove middle intials
 //format date
 //swap last 2 in order in pipe and comma
 //format output
 }
 
+function removeMI (str) {
+
+}
+ function formatPipeComma (str) {
+  console.log('here' ,str)
+ }
 
  
 console.log(fileParser())
